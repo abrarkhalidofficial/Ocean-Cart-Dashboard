0e6f8f9eeefd32b2721763b7f2cb45355279ed2f
Mehfooz-ur-Rehman <mehfoozijaz786@gmail.com>
2021-06-16 14:22:26 +0500

    commit

--- src/screens/DashboardPanel.js
+++ src/screens/DashboardPanel.js
@@ -31,14 +31,29 @@ const MessagePanelRow = () => {
   );
 };
 
+function StatsCard({ isData, data }) {
+  return (
+    <Card
+      heading="Lorem"
+      infoContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veniam."
+    >
+      {isData ? (
+        <div className="panel__card__content__statistics">{data}</div>
+      ) : (
+        <div className="panel__card__content__statistics__null">
+          Nothing Here
+        </div>
+      )}
+    </Card>
+  );
+}
 export default class DashboardPanel extends React.Component {
   render() {
     return (
       <div className="panel__conatainer__empty">
         <div className="panel__conatainer__empty__row">
           <Card
-            heading="Placed to Prepared:
-            "
+            heading="Placed to Prepared:"
             headingTimeStamp="0 days, 9 Hours"
             infoContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veniam."
             contentHeading="9"
@@ -95,60 +110,49 @@ export default class DashboardPanel extends React.Component {
           <Card
             heading="Lorem"
             infoContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veniam."
-            contentHeading="24,795"
-          />
-
+          >
+            <div className="panel__card__content__notes">
+              DB item:
+              <span>#OI-23434</span>
+            </div>
+            <div className="panel__card__content__notes">
+              DB item:
+              <span>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
+                illum beatae vero fugiat, sapiente omnis atque optio odio quasi
+                id recusandae excepturi ea accusamus, sunt amet explicabo
+                quisquam velit? Dignissimos?
+              </span>
+            </div>
+            <div className="panel__card__content__notes">
+              DB item:
+              <span>#OI-23434</span>
+            </div>
+            <div className="panel__card__content__notes">
+              DB item:
+              <a href="https://www.google.com/">https://www.google.com/</a>
+            </div>
+          </Card>
           <div className="panel__conatainer__empty__row__col">
             <div className="panel__conatainer__empty__row">
-              <Card
-                heading="Lorem"
-                infoContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veniam."
-                contentHeading="24,795"
-              />
-              <Card
-                heading="Lorem"
-                infoContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veniam."
-                contentHeading="24,795"
-              />
-              <Card
-                heading="Lorem"
-                infoContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veniam."
-                contentHeading="24,795"
-              />
+              <StatsCard isData={true} data="lorem" />
+              <StatsCard isData={true} />
+              <StatsCard isData={true} />
             </div>
             <div className="panel__conatainer__empty__row">
-              <Card
-                heading="Lorem"
-                infoContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veniam."
-                contentHeading="24,795"
-              />
-              <Card
-                heading="Lorem"
-                infoContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veniam."
-                contentHeading="24,795"
-              />
-              <Card
-                heading="Lorem"
-                infoContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veniam."
-                contentHeading="24,795"
-              />
+              <StatsCard isData={true} data="lorem" />
+              <StatsCard isData={false} />
+              <StatsCard isData={true} />
             </div>
             <div className="panel__conatainer__empty__row">
-              <Card
-                heading="Lorem"
-                infoContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veniam."
-                contentHeading="24,795"
+              <StatsCard isData={true} data="lorem" />
+              <StatsCard
+                isData={true}
+                data={
+                  <a href="https://www.google.com/">https://www.google.com/</a>
+                }
               />
-              <Card
-                heading="Lorem"
-                infoContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veniam."
-                contentHeading="24,795"
-              />
-              <Card
-                heading="Lorem"
-                infoContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veniam."
-                contentHeading="24,795"
-              />
+              <StatsCard isData={true} />
             </div>
           </div>
         </div>
