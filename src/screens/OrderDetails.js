eeed74ac415f29095e3daf144485dac46b24de74
Mehfooz-ur-Rehman <mehfoozijaz786@gmail.com>
2021-06-19 15:30:16 +0500

    commit

--- src/screens/OrderDetails.js
+++ src/screens/OrderDetails.js
@@ -6,25 +6,30 @@ import UserPic from "../assets/userPic.png";
 import { Link } from "react-router-dom";
 import { Nav } from "react-bootstrap";
 import Button from "../components/Button";
+import DetailsRow from "../components/DetailsRow";
+import InputboxComponent from "../components/Inputbox";
+import barcodePic from "../assets/barcodePic.png";
+import Select from "react-select";
 
-const MessagePanelRow = () => {
+const ProductList = () => {
   return (
-    <Nav.Link
-      to="/dashboard/messages"
-      as={Link}
-      className="message__panel__small__row"
-    >
-      <div className="message__panel__samll__content">
-        <img
-          src={UserPic}
-          alt="user-pic"
-          className="message__panel__small__row__img"
-        />
-        <div className="messenger__name" style={{ fontWeight: "800" }}>
-          Francesca Metts
+    <Nav.Link className="order__details__product__list__entry">
+      <div className="order__details__product__list__entry">
+        <div className="order__details__product__list__entry__left">
+          <img
+            src={productPic}
+            alt="productPic"
+            className="order__details__product__list__entry__img"
+          />
+          <div className="order__details__product__list__entry__name">
+            Product Name
+          </div>
+        </div>
+        <div className="order__details__product__list__entry__right">
+          <div className="order__details__product__list__entry__price">$35</div>
+          <Button placeholder="Delete" variant="btn-delete" />
         </div>
       </div>
-      <div className="messenger__name">3 Days Ago</div>
     </Nav.Link>
   );
 };
@@ -43,7 +48,16 @@ function StatsCard({ isData, data, heading, infoContent }) {
   );
 }
 export default class OrderDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { approved: false };
+  }
   render() {
+    const Options = [
+      { value: "Fixed Cart Discount", label: "Fixed Cart Discount" },
+      { value: "strawberry", label: "Strawberry" },
+      { value: "vanilla", label: "Vanilla" },
+    ];
     return (
       <div className="panel__conatainer__empty">
         <div className="panel__conatainer__empty__row">
@@ -51,9 +65,29 @@ export default class OrderDetails extends React.Component {
             <div className="order__details__header">
               <div className="order__details__heading">Order Details</div>
               <div className="order__details__cta">
-                <Button placeholder="Approve" variant="btn" />
+                {this.state.approved ? (
+                  <Button placeholder="Approved" variant="upload-btn" />
+                ) : (
+                  <Button
+                    placeholder="Approve"
+                    variant="btn"
+                    onClick={() => {
+                      this.setState({
+                        approved: true,
+                      });
+                    }}
+                  />
+                )}
                 <div className="order__details__cta__divider"></div>
-                <Button placeholder="Decline" variant="btn-delete" />
+                <Button
+                  placeholder="Decline"
+                  variant="btn-delete"
+                  onClick={() => {
+                    this.setState({
+                      approved: false,
+                    });
+                  }}
+                />
               </div>
             </div>
           </Card>
@@ -61,48 +95,205 @@ export default class OrderDetails extends React.Component {
         <div className="panel__conatainer__empty__row">
           <Card>
             <div className="order__details__product__list">
-              <div className="order__details__product__list__entry">
-                <div className="order__details__product__list__entry__left">
-                  <img
-                    src={productPic}
-                    alt="productPic"
-                    className="order__details__product__list__entry__img"
+              <ProductList />
+              <ProductList />
+              <ProductList />
+              <ProductList />
+              <ProductList />
+            </div>
+          </Card>
+          <Card>
+            <div className="order__details__customer">
+              <DetailsRow property="Customer" value="Wasif Awan Awan" />
+              <DetailsRow
+                property="Shipping Address"
+                value="3862  Clement Street Atlanta GA Georgia"
+              />
+              <DetailsRow
+                property="Note"
+                value={
+                  <InputboxComponent
+                    textarea="textarea"
+                    ClassName="input__box"
+                    placeholder="Note here"
                   />
-                  <div className="order__details__product__list__entry__name">
-                    Product Name
+                }
+              />
+              <DetailsRow
+                property="Customer Email"
+                value="awanwasif777@gmail.com"
+              />
+              <DetailsRow
+                property="Customer Phone No."
+                value="+92 242 24242424"
+              />
+              <DetailsRow
+                property="Billing Address"
+                value="Same as delivery address"
+              />
+              <DetailsRow
+                property="Amazon"
+                value={
+                  <div
+                    style={{
+                      display: "flex",
+                      marginBottom: ".5em",
+                    }}
+                  >
+                    <div style={{ width: "100px", marginRight: ".5em" }}>
+                      <Button placeholder="Prime" variant="upload-btn" />
+                    </div>
+                    <div style={{ width: "100px" }}>
+                      <Button placeholder="Prime" variant="upload-btn" />
+                    </div>
                   </div>
+                }
+              />
+              <DetailsRow
+                property="Barcode"
+                value={
+                  <img
+                    src={barcodePic}
+                    alt="barcodePic"
+                    style={{ width: "100px", marginBottom: ".5em" }}
+                  />
+                }
+              />
+              <DetailsRow
+                property="Amazon Link"
+                value={
+                  <Link to="" className="details__row__value__link">
+                    Link
+                  </Link>
+                }
+              />
+            </div>
+          </Card>
+        </div>
+        <div className="panel__conatainer__empty__row">
+          <Card>
+            <div className="order__details__shipping">
+              <div className="order__details__shipping__heading">
+                Shipment Tracking
+              </div>
+              <div className="order__details__shipping__status">
+                <div className="order__details__shipping__status__point">
+                  <div className="order__details__shipping__status__point__svg"></div>
+                  <div className="order__details__shipping__status__point__info"></div>
                 </div>
-                <div className="order__details__product__list__entry__right">
-                  <div className="order__details__product__list__entry__price">
-                    $35
-                  </div>
-                  <Button placeholder="Decline" variant="btn-delete" />
+                <div className="order__details__shipping__status__line"></div>
+                <div className="order__details__shipping__status__point">
+                  <div className="order__details__shipping__status__point__svg"></div>
+                  <div className="order__details__shipping__status__point__info"></div>
                 </div>
               </div>
             </div>
           </Card>
-          <Card />
         </div>
         <div className="panel__conatainer__empty__row">
-          <Card
-            heading="USA Consolidation Arrival To Departure: "
-            headingTimeStamp="2 days, 11 hours"
-            infoContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veniam."
-            contentHeading="59 hours"
-          />
-          <Card
-            heading="USA Consolidation Arrival To Departure:"
-            headingTimeStamp="2 days, 11 hours"
-            infoContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veniam."
-            contentHeading="54 hours"
-          />
-          <Card
-            heading="USA Consolidation Arrival To Departure:"
-            headingTimeStamp="2 days, 11 hours"
-            infoContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veniam."
-            contentHeading="54 hours"
-          />
+          <Card>
+            <div className="order__details__shipping__heading">
+              Shipping Status: Prepared
+            </div>
+            <DetailsRow property="Order Number" value="#2464" />
+            <DetailsRow property="Place on" value="14/04/2010" />
+            <DetailsRow property="Payment Type" value="Credit Card14/04/2010" />
+            <DetailsRow property="Checkout id" value="1313414" />
+            <DetailsRow property="User Email" value="alex@gmail.com" />
+            <DetailsRow property="User Phone" value="+123 45678902" />
+          </Card>
+          <Card>
+            <div className="order__details__customer">
+              <div className="order__details__shipping__heading">
+                Deliver To
+              </div>
+              <DetailsRow property="Customer" value="Wasif Awan Awan" />
+              <DetailsRow
+                property="Shipping Address"
+                value="3862  Clement Street Atlanta GA Georgia"
+              />
+              <DetailsRow
+                property="Note"
+                value={
+                  <InputboxComponent
+                    textarea="textarea"
+                    ClassName="input__box"
+                    placeholder="Note here"
+                  />
+                }
+              />
+              <DetailsRow
+                property="Customer Email"
+                value="awanwasif777@gmail.com"
+              />
+              <DetailsRow
+                property="Customer Phone No."
+                value="+92 242 24242424"
+              />
+              <DetailsRow
+                property="Billing Address"
+                value="Same as delivery address"
+              />
+              <DetailsRow property="Post/Zip Code" value="2343434" />
+            </div>
+          </Card>
         </div>
+        {this.state.approved ? (
+          <div className="panel__conatainer__empty__row">
+            <Card>
+              <div className="order__details__shipping__heading">
+                Card Transaction
+              </div>
+              <DetailsRow property="Authorized" value="Aur-162" />
+              <DetailsRow property="Created" value="14/04/2010" />
+              <DetailsRow property="UID" value="343789472389749" />
+              <DetailsRow property="Action ID" value="1313414" />
+              <DetailsRow property="Status" value="Approved" />
+              <DetailsRow property="Card" value="XXXX XXXX ---- ----" />
+              <DetailsRow property="Reference" value="Auth-che-45678902" />
+              <DetailsRow property="Auth Code" value="678902" />
+              <DetailsRow
+                property="Transaction for order ids"
+                value="678902678902"
+              />
+            </Card>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                flex: 1,
+              }}
+            >
+              <Card>
+                <div className="order__details__shipping__heading">
+                  Charge Card
+                </div>
+                <InputboxComponent
+                  type="text"
+                  ClassName="input__box"
+                  placeholder="Amount"
+                />
+                <div style={{ height: "1em" }}></div>
+                <Button placeholder="Charge" variant="btn" />
+              </Card>
+              <div style={{ height: "2em" }}></div>
+              <Card>
+                <div className="order__details__shipping__heading">
+                  Send Notification
+                </div>
+                <Select
+                  // defaultValue={[colourOptions[2], colourOptions[3]]}
+                  name="colors"
+                  options={Options}
+                  className="basic-multi-select"
+                  classNamePrefix="select"
+                />
+                <div style={{ height: "1em" }}></div>
+                <Button placeholder="Send Notification" variant="btn" />
+              </Card>
+            </div>
+          </div>
+        ) : null}
       </div>
     );
   }
